8a02f0a62dc42bf7a4876294e522cff285b393de
TheSilkMiner <thesilkminer@outlook.com>
2022-01-03 17:07:45 +0100

    Add test JAR
    Signed-off-by: TheSilkMiner <thesilkminer@outlook.com>

--- src/test/groovy/net/thesilkminer/mc/austinpowers/mojotest/AustinPowersMojoTest.groovy
+++ src/test/groovy/net/thesilkminer/mc/austinpowers/mojotest/AustinPowersMojoTest.groovy
@@ -1,13 +1,14 @@
 package net.thesilkminer.mc.austinpowers.mojotest
 
-import net.thesilkminer.mc.austinpowers.api.Mole
 import org.apache.logging.log4j.LogManager
 import org.apache.logging.log4j.Logger
 
 class AustinPowersMojoTest {
     private static final Logger LOGGER = LogManager.getLogger(AustinPowersMojoTest)
 
-    AustinPowersMojoTest(final Mole mole) {
-        LOGGER.info("Successfully loaded Groovy mod with mole '${mole}'")
+    AustinPowersMojoTest() {
+        LOGGER.info('Successfully loaded Groovy mod "{}"', this.toString())
+        LOGGER.info('Say hello to my meta-class {}', this.metaClass)
+        LOGGER.info('Buses are mojo "{}" and Forge "{}"', mojoBus, forgeBus)
     }
 }
